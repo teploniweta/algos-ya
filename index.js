2f239332abc27650add20f81f47c62be3aa0959e
teinawa <helepoctb123@gmail.com>
2023-11-14 17:14:07 +0300

    12/12

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 let fs = require('fs');
 fs.readFile('input.txt', 'utf8', function(error, fileContent){
     if(error) throw error;
-    console.log(fileContent);
-
-    let toWrite = fileContent + '228';
-
-    fs.writeFile('output.txt', toWrite, function(error){
+    const [a, b, c] = fileContent.split(/\s+/);
+    const ans = (Number(a) + Number(b) > Number(c))
+        && (Number(a) + Number(c) > Number(b))
+        && (Number(b) + Number(c) > Number(a));
+    fs.writeFile('output.txt', ans ? 'YES' : 'NO', function(error){
         if(error) throw error;
-        console.log(toWrite);
     });
 });
