eaefe3c4509e321912390bf556f782a9ca87b858
teinawa <helepoctb123@gmail.com>
2023-11-14 17:01:53 +0300

    5/6

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 let fs = require('fs');
 fs.readFile('input.txt', 'utf8', function(error, fileContent){
     if(error) throw error;
-    console.log(fileContent);
-
-    let toWrite = fileContent + '2218';
-
-    fs.writeFile('output.txt', toWrite, function(error){
+    const [tRoom, tCond, mode] = fileContent.split(/\s+/);
+    const ans = {
+        'heat': tRoom > tCond ? tRoom : tCond,
+        'freeze': tCond < tRoom ? tCond : tRoom,
+        'auto': tCond,
+        'fan': tRoom
+    }
+    fs.writeFile('output.txt', ans[mode], function(error){
         if(error) throw error;
-        console.log(toWrite);
     });
 });
