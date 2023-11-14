8771301f09f9d6da6769e22500241bc3f8ad5095
teinawa <helepoctb123@gmail.com>
2023-11-15 02:15:25 +0300

    14/14

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 let fs = require('fs');
 fs.readFile('input.txt', 'utf8', function(error, fileContent){
     if(error) throw error;
-    console.log(fileContent);
-
-    let toWrite = fileContent + '228';
-
-    fs.writeFile('output.txt', toWrite, function(error){
+    let ans = '';
+    const [a, b, c] = fileContent.split(/\s+/).map(el => Number(el));
+    if (c < 0) {
+        ans = "NO SOLUTION";
+    } else if ((a + b) === c * c && (2 * a + b) === c * c) {
+        ans = "MANY SOLUTIONS";
+    } else {
+        if (a !== 0 && (c * c - b) / a === Math.floor((c * c - b) / a)) {
+            ans = String(Math.floor((c * c - b) / a));
+        } else {
+            ans = "NO SOLUTION";
+        }
+    }
+    fs.writeFile('output.txt', ans, function(error){
         if(error) throw error;
-        console.log(toWrite);
+        console.log(ans);
     });
 });
