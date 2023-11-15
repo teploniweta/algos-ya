f998c7d1784c2af8ee0de683c77188ea40103482
teinawa <helepoctb123@gmail.com>
2023-11-15 17:15:48 +0300

    не учтены коллизии, надо доделать

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 let fs = require('fs');
 fs.readFile('input.txt', 'utf8', function(error, fileContent){
     if(error) throw error;
-    console.log(fileContent);
-
-    let toWrite = fileContent + '228';
-
-    fs.writeFile('output.txt', toWrite, function(error){
+    let ans = '';
+    const [k1, m, k2, p2, n2] = fileContent.split(/\s+/);
+    const flatsOnFloor = Math.ceil(k2 / n2);
+    const flatsOnEntrance = m * flatsOnFloor;
+    const p1 = m == 1 ? 0 : Math.ceil(k1 / (p2*flatsOnEntrance));
+    const n1 = flatsOnEntrance === Number(m) ? m : Math.ceil((k1-flatsOnEntrance) / flatsOnFloor);
+    fs.writeFile('output.txt', ans = `${p1} ${n1}`, function(error){
         if(error) throw error;
-        console.log(toWrite);
+        console.log(ans);
     });
 });
