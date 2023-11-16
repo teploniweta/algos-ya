e04185030f13247b18f3f6d1c96a69c412933455
teinawa <helepoctb123@gmail.com>
2023-11-16 19:38:06 +0300

    complete

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 let fs = require('fs');
 fs.readFile('input.txt', 'utf8', function(error, fileContent){
     if(error) throw error;
-    console.log(fileContent);
+    const [a, b, c] = fileContent.split(/\s+/).map(Number);
+    let ans = 0;
+    (function craftDetail (N, K, M) {
+        if (N && K && M) {
+            if (N < K || K < M) return;
+            let noUse = 0;
+            const count_K = Math.floor(N / K);
+            noUse += N % K;
+            const countMFromOneK = Math.floor(K / M);
+            const noUseFromOneK = K % M;
+            ans += countMFromOneK * count_K
+            noUse += noUseFromOneK * count_K
+            craftDetail(noUse, K, M)
+        }
+        else return;
+    })(a, b, c);
 
-    let toWrite = fileContent + '228';
-
-    fs.writeFile('output.txt', toWrite, function(error){
+    fs.writeFile('output.txt', String(ans), function(error){
         if(error) throw error;
-        console.log(toWrite);
+        console.log(String(ans));
     });
 });
