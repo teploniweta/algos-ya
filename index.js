b39c317b8c4a2dbc56dc09e46fcbe3176f503945
teinawa <helepoctb123@gmail.com>
2023-11-16 19:52:58 +0300

    переписал на делегирование генераторов

--- index.js
+++ index.js
@@ -2,24 +2,28 @@ let fs = require('fs');
 fs.readFile('input.txt', 'utf8', function(error, fileContent){
     if(error) throw error;
     const [a, b, c] = fileContent.split(/\s+/).map(Number);
-    let ans = 0;
-    (function craftDetail (N, K, M) {
-        if (N && K && M) {
-            if (N < K || K < M) return;
-            let noUse = 0;
-            const count_K = Math.floor(N / K);
-            noUse += N % K;
-            const countMFromOneK = Math.floor(K / M);
-            const noUseFromOneK = K % M;
-            ans += countMFromOneK * count_K
-            noUse += noUseFromOneK * count_K
-            craftDetail(noUse, K, M)
+    function* productionCycle(N, K, M) {
+        let ans = 0;
+        function* recursiveProduction(N, K, M) {
+            if (N && K && M) {
+                if (N < K || K < M) return;
+                let noUse = 0;
+                const count_K = Math.floor(N / K);
+                noUse += N % K;
+                const countMFromOneK = Math.floor(K / M);
+                const noUseFromOneK = K % M;
+                ans += countMFromOneK * count_K;
+                noUse += noUseFromOneK * count_K;
+                yield* recursiveProduction(noUse, K, M);
+            }
         }
-        else return;
-    })(a, b, c);
-
-    fs.writeFile('output.txt', String(ans), function(error){
+        yield* recursiveProduction(N, K, M);
+        return ans;
+    }
+    const generator = productionCycle(a, b, c);
+    const result = generator.next().value;
+    fs.writeFile('output.txt', String(result), function(error){
         if(error) throw error;
-        console.log(String(ans));
+        console.log(String(result));
     });
 });
