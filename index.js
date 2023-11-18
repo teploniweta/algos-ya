ea5708d20ba13ebd8059f9ea31f5892976715c67
teinawa <helepoctb123@gmail.com>
2023-11-18 22:28:43 +0300

    complete)))

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 let fs = require('fs');
 fs.readFile('input.txt', 'utf8', function(error, fileContent){
     if(error) throw error;
-    console.log(fileContent);
-
-    let toWrite = fileContent + '228';
-
-    fs.writeFile('output.txt', toWrite, function(error){
+    const [a, b, n, m] = fileContent.split(/\s+/).map((el) => Number(el));
+    let ans = '';
+    const min1 = (a + 1) * (n - 1) + 1;
+    const max1 = (a + 1) * (n - 1) + 1 + 2 * a;
+    const min2 = (b + 1) * (m - 1) + 1;
+    const max2 = (b + 1) * (m - 1) + 1 + 2 * b;
+    const minAns = Math.max(min1, min2);
+    const maxAns = Math.min(max1, max2);
+    if (maxAns < minAns) ans = '-1'
+    else ans = `${minAns} ${maxAns}`
+    fs.writeFile('output.txt', ans, function(error){
         if(error) throw error;
-        console.log(toWrite);
+        console.log(ans);
     });
 });
