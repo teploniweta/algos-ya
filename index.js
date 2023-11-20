a6f769cec28faba0baba1d3eacb556b4ba9958f3
teinawa <helepoctb123@gmail.com>
2023-11-20 20:47:22 +0300

    complete

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 let fs = require('fs');
 fs.readFile('input.txt', 'utf8', function(error, fileContent){
     if(error) throw error;
-    console.log(fileContent);
-
-    let toWrite = fileContent + '228';
-
-    fs.writeFile('output.txt', toWrite, function(error){
+    const [a, b, c, d, e] = fileContent.split(/\s+/).map(Number);
+    const arr1 = [a, b, c].sort((a,b) => a-b);
+    const arr2 = [d, e].sort((a,b) => a-b);
+    const ans = arr1[0] <= arr2[0] && arr1[0] <= arr2[0] ? "YES" : "NO";
+    fs.writeFile('output.txt', ans, function(error){
         if(error) throw error;
-        console.log(toWrite);
+        console.log(ans);
     });
 });
