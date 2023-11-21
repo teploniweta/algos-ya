9d4650e5795510354258e890879a82ff6f02132f
teinawa <helepoctb123@gmail.com>
2023-11-22 00:03:52 +0300

    complete

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 let fs = require('fs');
 fs.readFile('input.txt', 'utf8', function(error, fileContent){
     if(error) throw error;
-    console.log(fileContent);
-
-    let toWrite = fileContent + '228';
-
-    fs.writeFile('output.txt', toWrite, function(error){
+    // линейная сложность
+    const obj = {};
+    const n = fileContent.split(/\s+/).slice(0, -1);
+    n.forEach(e => {
+        if (!(e in obj)) obj[e] = true
+    })
+    const ans = String(Object.keys(obj).length);
+    fs.writeFile('output.txt', ans, function(error){
         if(error) throw error;
-        console.log(toWrite);
+        console.log(ans);
     });
 });
