8a050d96d74987f9921dd9d43142e6bbca401ed0
teinawa <helepoctb123@gmail.com>
2023-11-22 00:47:54 +0300

    complete

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 let fs = require('fs');
 fs.readFile('input.txt', 'utf8', function(error, fileContent){
     if(error) throw error;
-    // линейная сложность
-    const obj = {};
-    const n = fileContent.split(/\s+/).slice(0, -1);
-    n.forEach(e => {
-        if (!(e in obj)) obj[e] = true
-    })
-    const ans = String(Object.keys(obj).length);
+    const rows = fileContent.split('\n').map(row => row.split(' ').map(Number));
+    const set = new Set(rows[1]); //  O(n)
+    const ans = rows[0]
+        .filter(el => set.has(el)) //  O(m)
+        .sort((a, b) => a - b) // O(m log m)
+        .join(" ");
+    // общая сложность  O(n) + O(m) + O(m log m) что в итоге можно упростить до O(m log m), т.к. m определяющий член
     fs.writeFile('output.txt', ans, function(error){
         if(error) throw error;
         console.log(ans);
